8abe338132077f3b3418a2c9073f25a395a94130
qdvu124 <vudinhquan124@gmail.com>
2016-11-26 22:44:36 +0000

    Working

--- mars/Position.java
+++ mars/Position.java
@@ -14,9 +14,11 @@ public class Position implements State {
 	protected final int currentColumn;
 	protected final int width;
 	protected final int length;
+	protected final int moves;
 	
-	public Position (int[][] map, Cell currentCell) {
+	public Position (int[][] map, Cell currentCell, int moves) {
 		this.map = map;
+		this.moves = moves;
 		currentRow = currentCell.getRow();
 		currentColumn = currentCell.getColumn();
 		width = map.length;
@@ -26,15 +28,40 @@ public class Position implements State {
 	@Override
 	public Set<? extends Action> getApplicableActions() {
 		// TODO Auto-generated method stub
+		if(moves == 0)
+			return null;
 		Set<Action> actions = new LinkedHashSet<Action>();
-		
+		for (Movement movement : Movement.values()) {
+			int newRow = currentRow + movement.deltaRow;
+			int newColumn = currentColumn + movement.deltaColumn;
+			if (0 <= newRow && newRow < width && 0 <= newColumn && newColumn < length)
+				if(map[newRow][newColumn] == 0)
+					actions.add(movement);
+		}
 		return actions;
 	}
 
 	@Override
 	public State getActionResult(Action action) {
 		// TODO Auto-generated method stub
-		return null;
+		Movement movement = (Movement) action;
+		int newRow = currentRow + movement.deltaRow;
+		int newColumn = currentColumn + movement.deltaColumn;
+		return new Position(this.map, new Cell(newRow, newColumn), moves - 1);
+	}
+	
+	public int hashCode() {
+		return currentRow * 2 + currentColumn * 3 + visitedCells.size() * 5;
+	}
+	
+	public boolean equals(Object object) {
+		Position other = (Position) object;
+		for(Cell cell: visitedCells)
+			if(!other.visitedCells.contains(cell))
+				return false;
+		if(other.moves != this.moves)
+			return false;
+		return true;
 	}
 
 }
